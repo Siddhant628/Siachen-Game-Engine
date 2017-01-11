9a44a0b9a7fe2e559bbd111185d66744f5bc2b1c
Siddhant Grover <siddhantgrover94@hotmail.com>
2017-01-11 14:42:03 -0500

    - Added placeholder code for OpenGL version - The entire solution builds now

---
+++ SiachenGameEngine/source/Game.Desktop.OpenGL/Main.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nCmdShow);
+}
