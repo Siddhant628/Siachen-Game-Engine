5a46e97b66e73ae29165a2f3efcb229e0aca89e8
Siddhant Grover <siddhantgrover94@hotmail.com>
2017-01-12 22:40:22 -0500

    - Added unit tests for memory leak - Batch build is working now

--- SiachenGameEngine/source/UnitTest.Library.Desktop/SListTest.cpp
+++ SiachenGameEngine/source/UnitTest.Library.Desktop/SListTest.cpp
@@ -9,13 +9,32 @@ namespace UnitTestLibraryDesktop
 	TEST_CLASS(SListTest)
 	{
 	public:
-		
+		TEST_METHOD_INITIALIZE(Initialize)
+		{
+			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
+			_CrtMemCheckpoint(&sStartMemState);
+		}
+
+		TEST_METHOD_CLEANUP(Cleanup)
+		{
+			_CrtMemState endMemState, diffMemState;
+			_CrtMemCheckpoint(&endMemState);
+			if (_CrtMemDifference(&diffMemState, &sStartMemState, &endMemState))
+			{
+				_CrtMemDumpStatistics(&diffMemState);
+				Assert::Fail(L"Memory Leaks!");
+			}
+		}
+
 		TEST_METHOD(SListConstructors)
 		{
 			int data = 1;
 			SiachenGameEngine::SList list(data);
 			Assert::AreEqual(data, list.GetData());
 		}
-
+	private:
+		static _CrtMemState sStartMemState;
 	};
+
+	_CrtMemState SListTest::sStartMemState;
 }
