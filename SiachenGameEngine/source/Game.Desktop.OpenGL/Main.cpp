8fa90c721998e7204fdf2fe19e86fc6e6e835824
Siddhant Grover <siddhantgrover94@hotmail.com>
2017-01-12 19:51:40 -0500

    - Started work on OpenGL version

--- SiachenGameEngine/source/Game.Desktop.OpenGL/Main.cpp
+++ SiachenGameEngine/source/Game.Desktop.OpenGL/Main.cpp
@@ -1,12 +1,44 @@
 #include "pch.h"
 
-
-#include "glfw3.h"
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hInstance);
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(nCmdShow);
+	
+	//const glm::vec4 CornflowerBlue = glm::vec4(0.392f, 0.584f, 0.929f, 1.0f);
+
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Essentials", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (gl3wInit() != 0)
+	{
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//glClearBufferfv(GL_COLOR, 0, &CornflowerBlue[0]);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
--- SiachenGameEngine/source/Game.Desktop.OpenGL/pch.h
+++ SiachenGameEngine/source/Game.Desktop.OpenGL/pch.h
@@ -1,3 +1,25 @@
 #pragma once
 
 #include <windows.h>
+#include <exception>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+
+#include "GL/gl3w.h"
+#include "GLFW/glfw3.h"
+
+// TODO Check why glm libraries are producing warnings
+//#include "glm/glm.hpp"
+//#include "glm/gtx/simd_mat4.hpp"
+//#include "glm/gtc/matrix_transform.hpp"
+//#include "glm/gtc/matrix_inverse.hpp"
+//#include "glm/gtc/type_ptr.hpp"
+
+// TODO Check if this file is obsolete
+//#include "glm/core/func_trigonometric.hpp"
+
+#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_WGL
