6d6b74df00c5b0297cd386162151e4dda03f3599
Siddhant Grover <siddhantgrover94@hotmail.com>
2017-02-09 13:34:34 -0500

    - Integrated GLM math library. - Integrated the RTTI header. - Started working on the Datum.

--- SiachenGameEngine/source/Game.Desktop.OpenGL/pch.h
+++ SiachenGameEngine/source/Game.Desktop.OpenGL/pch.h
@@ -11,5 +11,8 @@
 #include "GL/gl3w.h"
 #include "GLFW/glfw3.h"
 
+#define GLM_FORCE_CXX11
+#include "glm/glm.hpp"
+
 #define GLFW_EXPOSE_NATIVE_WIN32
 #define GLFW_EXPOSE_NATIVE_WGL
---
+++ SiachenGameEngine/source/Library.Shared/Datum.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace SiachenGameEngine
+{
+	namespace Library
+	{
+		enum class DatumType
+		{
+			Unknown,
+			Integer,
+			Float,
+			Vector,
+			Matrix,
+			Table,
+			String,
+			Pointer
+		};
+
+		class Datum
+		{
+
+		};
+	}
+}
