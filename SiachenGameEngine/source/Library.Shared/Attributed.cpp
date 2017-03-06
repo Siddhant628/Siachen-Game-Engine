6a9d7b584c760082b91748f590b2a20c1b72cc37
Siddhant Grover <siddhantgrover94@hotmail.com>
2017-03-06 12:53:13 -0500

    - Added overloads for AddExternalAttribute method.

--- SiachenGameEngine/source/Library.Shared/Attributed.cpp
+++ SiachenGameEngine/source/Library.Shared/Attributed.cpp
@@ -19,26 +19,89 @@ namespace SiachenGameEngine
 			UpdatePrescribedAttributeCache();
 		}
 
+
 		void Attributed::Populate()
 		{
 			AddInternalAttribute("this", static_cast<RTTI*>(this));
 		}
 
-		void Attributed::AddExternalAttribute(const std::string& attributeName, const std::int32_t* externalAttribute, std::uint32_t numberOfValues/*, bool isPrescribedAttribute*/)
+		void Attributed::UpdatePrescribedAttributeCache()
+		{
+			Vector<std::string> attributeList;
+			GetKeys(attributeList);
+			// Update prescribed attribute count
+			mPrescribedAttributeCount = attributeList.Size();
+			// Insert <classID, Vector of strings> pair into static hashmap if absent
+			CacheHashmapType::Iterator itCache = sPrescribedAttributeCache.Find(TypeIdInstance());
+			if (itCache == sPrescribedAttributeCache.end())
+			{
+				std::pair<std::uint64_t, Containers::Vector<std::string>> pair(TypeIdInstance(), Vector<std::string>());
+				sPrescribedAttributeCache.Insert(pair);
+				itCache = sPrescribedAttributeCache.Find(TypeIdInstance());
+			}
+			// Fill the prescribed attributes vector if it is empty
+			for (std::uint32_t i = 0; i < mPrescribedAttributeCount; ++i)
+			{
+				std::string attributeName = attributeList.At(i);
+				if ((*itCache).second.Find(attributeName) == (*itCache).second.end())
+				{
+					(*itCache).second.PushBack(attributeName);
+				}
+			}
+		}
+
+
+		void Attributed::AddExternalAttribute(const std::string& attributeName, const std::int32_t* externalAttribute, std::uint32_t numberOfValues)
 		{
 			Datum& datum = Append(attributeName);
 			datum.SetType(DatumType::IntegerType);
 			datum.SetStorage(const_cast<std::int32_t*>(externalAttribute), numberOfValues);
 		}
 
-		void Attributed::AddExternalAttribute(const std::string& attributeName, const std::float_t* externalAttribute, std::uint32_t numberOfValues/*, bool isPrescribedAttribute*/)
+		void Attributed::AddExternalAttribute(const std::string& attributeName, const std::float_t* externalAttribute, std::uint32_t numberOfValues)
 		{
 			Datum& datum = Append(attributeName);
 			datum.SetType(DatumType::FloatType);
 			datum.SetStorage(const_cast<std::float_t*>(externalAttribute), numberOfValues);
 		}
 
-		void Attributed::AddInternalAttribute(const std::string& attributeName, const RTTI* attributeValue/*, bool isPrescribedAttribute*/)
+		void Attributed::AddExternalAttribute(const std::string& attributeName, const std::string* externalAttribute, std::uint32_t numberOfValues)
+		{
+			Datum& datum = Append(attributeName);
+			datum.SetType(DatumType::StringType);
+			datum.SetStorage(const_cast<std::string*>(externalAttribute), numberOfValues);
+		}
+
+		void Attributed::AddExternalAttribute(const std::string& attributeName, const glm::vec4* externalAttribute, std::uint32_t numberOfValues)
+		{
+			Datum& datum = Append(attributeName);
+			datum.SetType(DatumType::VectorType);
+			datum.SetStorage(const_cast<glm::vec4*>(externalAttribute), numberOfValues);
+		}
+
+		void Attributed::AddExternalAttribute(const std::string& attributeName, const glm::mat4x4* externalAttribute, std::uint32_t numberOfValues)
+		{
+			Datum& datum = Append(attributeName);
+			datum.SetType(DatumType::MatrixType);
+			datum.SetStorage(const_cast<glm::mat4x4*>(externalAttribute), numberOfValues);
+		}
+
+		void Attributed::AddExternalAttribute(const std::string& attributeName, const Scope** externalAttribute, std::uint32_t numberOfValues)
+		{
+			Datum& datum = Append(attributeName);
+			datum.SetType(DatumType::TableType);
+			datum.SetStorage(const_cast<Scope**>(externalAttribute), numberOfValues);
+		}
+
+		void Attributed::AddExternalAttribute(const std::string& attributeName, const RTTI** externalAttribute, std::uint32_t numberOfValues)
+		{
+			Datum& datum = Append(attributeName);
+			datum.SetType(DatumType::PointerType);
+			datum.SetStorage(const_cast<RTTI**>(externalAttribute), numberOfValues);
+		}
+
+
+		void Attributed::AddInternalAttribute(const std::string& attributeName, const std::int32_t attributeValue)
 		{
 			Datum& datum = Append(attributeName);
 			if (datum.IsEmpty())
@@ -50,6 +113,85 @@ namespace SiachenGameEngine
 				datum.Set(attributeValue);
 			}
 		}
+
+		void Attributed::AddInternalAttribute(const std::string& attributeName, const std::float_t attributeValue)
+		{
+			Datum& datum = Append(attributeName);
+			if (datum.IsEmpty())
+			{
+				datum.PushBack(attributeValue);
+			}
+			else
+			{
+				datum.Set(attributeValue);
+			}
+		}
+
+		void Attributed::AddInternalAttribute(const std::string& attributeName, const std::string& attributeValue)
+		{
+			Datum& datum = Append(attributeName);
+			if (datum.IsEmpty())
+			{
+				datum.PushBack(attributeValue);
+			}
+			else
+			{
+				datum.Set(attributeValue);
+			}
+		}
+
+		void Attributed::AddInternalAttribute(const std::string& attributeName, const glm::vec4& attributeValue)
+		{
+			Datum& datum = Append(attributeName);
+			if (datum.IsEmpty())
+			{
+				datum.PushBack(attributeValue);
+			}
+			else
+			{
+				datum.Set(attributeValue);
+			}
+		}
+
+		void Attributed::AddInternalAttribute(const std::string& attributeName, const glm::mat4x4& attributeValue)
+		{
+			Datum& datum = Append(attributeName);
+			if (datum.IsEmpty())
+			{
+				datum.PushBack(attributeValue);
+			}
+			else
+			{
+				datum.Set(attributeValue);
+			}
+		}
+
+		void Attributed::AddInternalAttribute(const std::string& attributeName, const Scope* attributeValue)
+		{
+			Datum& datum = Append(attributeName);
+			if (datum.IsEmpty())
+			{
+				datum.PushBack(attributeValue);
+			}
+			else
+			{
+				datum.Set(attributeValue);
+			}
+		}
+
+		void Attributed::AddInternalAttribute(const std::string& attributeName, const RTTI* attributeValue)
+		{
+			Datum& datum = Append(attributeName);
+			if (datum.IsEmpty())
+			{
+				datum.PushBack(attributeValue);
+			}
+			else
+			{
+				datum.Set(attributeValue);
+			}
+		}
+
 
 		bool Attributed::IsAttribute(const std::string& attributeName) const
 		{
@@ -77,6 +219,7 @@ namespace SiachenGameEngine
 			return false;
 		}
 
+
 		Containers::Datum& Attributed::AppendAuxiliaryAttribute(const std::string& attributeName)
 		{
 			if (IsPrescribedAttribute(attributeName))
@@ -90,31 +233,6 @@ namespace SiachenGameEngine
 		std::uint32_t Attributed::AuxiliaryBegin() const
 		{
 			return mPrescribedAttributeCount;
-		}
-
-		void Attributed::UpdatePrescribedAttributeCache()
-		{
-			Vector<std::string> attributeList;
-			GetKeys(attributeList);
-			// Update prescribed attribute count
-			mPrescribedAttributeCount = attributeList.Size();
-			// Insert <classID, Vector of strings> pair into static hashmap if absent
-			CacheHashmapType::Iterator itCache = sPrescribedAttributeCache.Find(TypeIdInstance());
-			if (itCache == sPrescribedAttributeCache.end())
-			{
-				std::pair<std::uint64_t, Containers::Vector<std::string>> pair(TypeIdInstance(), Vector<std::string>());
-				sPrescribedAttributeCache.Insert(pair);
-				itCache = sPrescribedAttributeCache.Find(TypeIdInstance());
-			}
-			// Fill the prescribed attributes vector if it is empty
-			for (std::uint32_t i = 0; i < mPrescribedAttributeCount; ++i)
-			{
-				std::string attributeName = attributeList.At(i);
-				if ((*itCache).second.Find(attributeName) == (*itCache).second.end())
-				{
-					(*itCache).second.PushBack(attributeName);
-				}
-			}
 		}
 	}
 
