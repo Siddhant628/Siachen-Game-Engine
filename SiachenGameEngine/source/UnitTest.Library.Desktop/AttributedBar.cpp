586f9afac9a15dd6c478f70ccf9e7b8d6a82d0df
Siddhant Grover <siddhantgrover94@hotmail.com>
2017-03-22 12:35:44 -0400

    - Reintegrated Attributed and Scope with the modified version of Datum.

--- SiachenGameEngine/source/UnitTest.Library.Desktop/AttributedBar.cpp
+++ SiachenGameEngine/source/UnitTest.Library.Desktop/AttributedBar.cpp
@@ -1,133 +1,133 @@
 #include "pch.h"
-//
-//#include "Attributed.h"
-//#include "AttributedBar.h"
-//#include "AttributedFoo.h"
-//#include "RTTI.h"
-//#include "Foo.h"
-//
-//using namespace SiachenGameEngine::GameplayFramework;
-//
-//namespace SiachenGameEngine
-//{
-//	namespace HelperClasses
-//	{
-//		RTTI_DEFINITIONS(AttributedBar)
-//
-//		AttributedBar::AttributedBar()
-//		{
-//			mRTTI = new Foo(100);
-//			Populate();
-//			UpdatePrescribedAttributeInfo();
-//		}
-//
-//		AttributedBar::AttributedBar(const AttributedBar& rhs) : AttributedFoo(rhs)
-//		{
-//
-//			(*this)["mFloat"].SetStorage(&mFloat, 1U);
-//			(*this)["mString"].SetStorage(&mString, 1U);
-//			(*this)["mVec4"].SetStorage(&mVec4, 1U);
-//			(*this)["mMat4x4"].SetStorage(&mMat4x4, 1U);
-//
-//			mRTTI = new Foo(rhs.GetRTTIFoo());
-//			(*this)["mRTTI"].SetStorage(&mRTTI, 1U);
-//
-//			(*this)["mFloat"].Set(rhs.GetFloat());
-//			(*this)["mString"].Set(rhs.GetString());
-//			(*this)["mVec4"].Set(rhs.GetVector());
-//			(*this)["mMat4x4"].Set(rhs.GetMatrix());
-//
-//			SetAuxillaryBegin(rhs.AuxiliaryBegin());
-//		}
-//
-//		AttributedBar& AttributedBar::operator=(const AttributedBar& rhs)
-//		{
-//			if (this != &rhs)
-//			{
-//				AttributedFoo::operator=(rhs);
-//
-//				(*this)["mFloat"].SetStorage(&mFloat, 1U);
-//				(*this)["mString"].SetStorage(&mString, 1U);
-//				(*this)["mVec4"].SetStorage(&mVec4, 1U);
-//				(*this)["mMat4x4"].SetStorage(&mMat4x4, 1U);
-//
-//				*mRTTI = *rhs.mRTTI;
-//				(*this)["mRTTI"].SetStorage(&mRTTI, 1U);
-//
-//				(*this)["mFloat"].Set(rhs.GetFloat());
-//				(*this)["mString"].Set(rhs.GetString());
-//				(*this)["mVec4"].Set(rhs.GetVector());
-//				(*this)["mMat4x4"].Set(rhs.GetMatrix());
-//
-//				SetAuxillaryBegin(rhs.AuxiliaryBegin());
-//			}
-//			return *this;
-//		}
-//
-//		AttributedBar::~AttributedBar()
-//		{
-//			delete mRTTI;
-//		}
-//
-//		void AttributedBar::Populate()
-//		{
-//			AddExternalAttribute("mFloat", &mFloat, 1U);
-//			AddExternalAttribute("mString", &mString, 1U);
-//			AddExternalAttribute("mVec4", &mVec4, 1U);
-//			AddExternalAttribute("mMat4x4", &mMat4x4, 1U);
-//			AddExternalAttribute("mRTTI", &mRTTI, 1U);
-//		}
-//
-//		std::float_t AttributedBar::GetFloat() const
-//		{
-//			return mFloat;
-//		}
-//
-//		const std::string AttributedBar::GetString() const
-//		{
-//			return mString;
-//		}
-//
-//		glm::vec4 AttributedBar::GetVector() const
-//		{
-//			return mVec4;
-//		}
-//
-//		glm::mat4x4 AttributedBar::GetMatrix() const
-//		{
-//			return mMat4x4;
-//		}
-//
-//		std::int32_t AttributedBar::GetRTTIFoo() const
-//		{
-//			Foo* pFoo = mRTTI->As<Foo>();
-//			return pFoo->GetValue();
-//		}
-//
-//		void AttributedBar::SetFloat(std::float_t value)
-//		{
-//			mFloat = value;
-//		}
-//
-//		void AttributedBar::SetString(const std::string& str)
-//		{
-//			mString = str;
-//		}
-//
-//		void AttributedBar::SetVector(glm::vec4& vec)
-//		{
-//			mVec4 = vec;
-//		}
-//
-//		void AttributedBar::SetMatrix(glm::mat4x4& matrix)
-//		{
-//			mMat4x4 = matrix;
-//		}
-//
-//		void AttributedBar::SetRTTIFoo(std::int32_t value)
-//		{
-//			Foo* pFoo = mRTTI->As<Foo>();
-//			pFoo->SetValue(value);
-//		}
-//	}
-//}
+
+#include "Attributed.h"
+#include "AttributedBar.h"
+#include "AttributedFoo.h"
+#include "RTTI.h"
+#include "Foo.h"
+
+using namespace SiachenGameEngine::GameplayFramework;
+
+namespace SiachenGameEngine
+{
+	namespace HelperClasses
+	{
+		RTTI_DEFINITIONS(AttributedBar)
+
+		AttributedBar::AttributedBar()
+		{
+			mRTTI = new Foo(100);
+			Populate();
+			UpdatePrescribedAttributeInfo();
+		}
+
+		AttributedBar::AttributedBar(const AttributedBar& rhs) : AttributedFoo(rhs)
+		{
+
+			(*this)["mFloat"].SetStorage(&mFloat, 1U);
+			(*this)["mString"].SetStorage(&mString, 1U);
+			(*this)["mVec4"].SetStorage(&mVec4, 1U);
+			(*this)["mMat4x4"].SetStorage(&mMat4x4, 1U);
+
+			mRTTI = new Foo(rhs.GetRTTIFoo());
+			(*this)["mRTTI"].SetStorage(&mRTTI, 1U);
+
+			(*this)["mFloat"].Set(rhs.GetFloat());
+			(*this)["mString"].Set(rhs.GetString());
+			(*this)["mVec4"].Set(rhs.GetVector());
+			(*this)["mMat4x4"].Set(rhs.GetMatrix());
+
+			SetAuxillaryBegin(rhs.AuxiliaryBegin());
+		}
+
+		AttributedBar& AttributedBar::operator=(const AttributedBar& rhs)
+		{
+			if (this != &rhs)
+			{
+				AttributedFoo::operator=(rhs);
+
+				(*this)["mFloat"].SetStorage(&mFloat, 1U);
+				(*this)["mString"].SetStorage(&mString, 1U);
+				(*this)["mVec4"].SetStorage(&mVec4, 1U);
+				(*this)["mMat4x4"].SetStorage(&mMat4x4, 1U);
+
+				*mRTTI = *rhs.mRTTI;
+				(*this)["mRTTI"].SetStorage(&mRTTI, 1U);
+
+				(*this)["mFloat"].Set(rhs.GetFloat());
+				(*this)["mString"].Set(rhs.GetString());
+				(*this)["mVec4"].Set(rhs.GetVector());
+				(*this)["mMat4x4"].Set(rhs.GetMatrix());
+
+				SetAuxillaryBegin(rhs.AuxiliaryBegin());
+			}
+			return *this;
+		}
+
+		AttributedBar::~AttributedBar()
+		{
+			delete mRTTI;
+		}
+
+		void AttributedBar::Populate()
+		{
+			AddExternalAttribute("mFloat", &mFloat, 1U);
+			AddExternalAttribute("mString", &mString, 1U);
+			AddExternalAttribute("mVec4", &mVec4, 1U);
+			AddExternalAttribute("mMat4x4", &mMat4x4, 1U);
+			AddExternalAttribute("mRTTI", &mRTTI, 1U);
+		}
+
+		std::float_t AttributedBar::GetFloat() const
+		{
+			return mFloat;
+		}
+
+		const std::string AttributedBar::GetString() const
+		{
+			return mString;
+		}
+
+		glm::vec4 AttributedBar::GetVector() const
+		{
+			return mVec4;
+		}
+
+		glm::mat4x4 AttributedBar::GetMatrix() const
+		{
+			return mMat4x4;
+		}
+
+		std::int32_t AttributedBar::GetRTTIFoo() const
+		{
+			Foo* pFoo = mRTTI->As<Foo>();
+			return pFoo->GetValue();
+		}
+
+		void AttributedBar::SetFloat(std::float_t value)
+		{
+			mFloat = value;
+		}
+
+		void AttributedBar::SetString(const std::string& str)
+		{
+			mString = str;
+		}
+
+		void AttributedBar::SetVector(glm::vec4& vec)
+		{
+			mVec4 = vec;
+		}
+
+		void AttributedBar::SetMatrix(glm::mat4x4& matrix)
+		{
+			mMat4x4 = matrix;
+		}
+
+		void AttributedBar::SetRTTIFoo(std::int32_t value)
+		{
+			Foo* pFoo = mRTTI->As<Foo>();
+			pFoo->SetValue(value);
+		}
+	}
+}
--- SiachenGameEngine/source/UnitTest.Library.Desktop/AttributedFoo.cpp
+++ SiachenGameEngine/source/UnitTest.Library.Desktop/AttributedFoo.cpp
@@ -1,56 +1,56 @@
 #include "pch.h"
-//
-//#include "AttributedFoo.h"
-//#include "RTTI.h"
-//
-//namespace SiachenGameEngine
-//{
-//	namespace HelperClasses
-//	{
-//		RTTI_DEFINITIONS(AttributedFoo)
-//
-//		AttributedFoo::AttributedFoo()
-//		{
-//			Populate();
-//			UpdatePrescribedAttributeInfo();
-//		}
-//
-//		AttributedFoo::AttributedFoo(const AttributedFoo& rhs) : Attributed(rhs)
-//		{
-//
-//			(*this)["mInteger"].SetStorage(&mInteger, 1U);
-//			(*this)["mInteger"].Set(rhs.GetInteger());
-//
-//			SetAuxillaryBegin(rhs.AuxiliaryBegin());
-//		}
-//
-//		AttributedFoo& AttributedFoo::operator=(const AttributedFoo& rhs)
-//		{
-//			if (this != &rhs)
-//			{
-//				Attributed::operator=(rhs);
-//
-//				(*this)["mInteger"].SetStorage(&mInteger, 1U);
-//				(*this)["mInteger"].Set(rhs.GetInteger());
-//
-//				SetAuxillaryBegin(rhs.AuxiliaryBegin());
-//			}
-//			return *this;
-//		}
-//
-//		void AttributedFoo::Populate()
-//		{
-//			AddExternalAttribute("mInteger", &mInteger, 1U);
-//		}
-//
-//		std::int32_t AttributedFoo::GetInteger() const
-//		{
-//			return mInteger;
-//		}
-//
-//		void AttributedFoo::SetInteger(std::int32_t value)
-//		{
-//			mInteger = value;
-//		}
-//	}
-//}
+
+#include "AttributedFoo.h"
+#include "RTTI.h"
+
+namespace SiachenGameEngine
+{
+	namespace HelperClasses
+	{
+		RTTI_DEFINITIONS(AttributedFoo)
+
+		AttributedFoo::AttributedFoo()
+		{
+			Populate();
+			UpdatePrescribedAttributeInfo();
+		}
+
+		AttributedFoo::AttributedFoo(const AttributedFoo& rhs) : Attributed(rhs)
+		{
+
+			(*this)["mInteger"].SetStorage(&mInteger, 1U);
+			(*this)["mInteger"].Set(rhs.GetInteger());
+
+			SetAuxillaryBegin(rhs.AuxiliaryBegin());
+		}
+
+		AttributedFoo& AttributedFoo::operator=(const AttributedFoo& rhs)
+		{
+			if (this != &rhs)
+			{
+				Attributed::operator=(rhs);
+
+				(*this)["mInteger"].SetStorage(&mInteger, 1U);
+				(*this)["mInteger"].Set(rhs.GetInteger());
+
+				SetAuxillaryBegin(rhs.AuxiliaryBegin());
+			}
+			return *this;
+		}
+
+		void AttributedFoo::Populate()
+		{
+			AddExternalAttribute("mInteger", &mInteger, 1U);
+		}
+
+		std::int32_t AttributedFoo::GetInteger() const
+		{
+			return mInteger;
+		}
+
+		void AttributedFoo::SetInteger(std::int32_t value)
+		{
+			mInteger = value;
+		}
+	}
+}
