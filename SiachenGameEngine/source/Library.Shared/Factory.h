4e448036ac2f0f683f331d813e1c00f45591d8b1
Siddhant Grover <siddhantgrover94@hotmail.com>
2017-03-27 22:55:30 -0400

    - Cleanup and commenting of Factor.

--- SiachenGameEngine/source/Library.Shared/Factory.h
+++ SiachenGameEngine/source/Library.Shared/Factory.h
@@ -18,16 +18,16 @@ namespace SiachenGameEngine
 			virtual std::string ClassName() const = 0;
 		private:
 			/**
-			* Creates a concrete product associated with a concrete factory.
-			* @return The address of the concrete product.
+			* Creates a concrete product associated with a concrete product factory.
+			* @return The address of the concrete product created on heap.
 			*/
 			virtual AbstractProductT* Create() = 0;
 
 			// Manager (Singleton)
 		public:
 			/**
-			* Find the concrete factory associate with a specific class.
-			* @param className The name of the class whose associated concrete factory has to be found.
+			* Find the concrete product factory associate with a specific class.
+			* @param className The name of the class whose associated concrete product factory has to be found.
 			* @return The address of the factory which produces instances of class with the name className.
 			*/
 			static Factory<AbstractProductT>* Find(const std::string& className);
@@ -37,22 +37,31 @@ namespace SiachenGameEngine
 			* @return The address of the concrete product.
 			*/
 			static AbstractProductT* Create(const std::string& className);
-
+			/**
+			* Get an iterator at the beginning of the container of factories.
+			* @return The begin iterator of the static hashmap mFactoryTable.
+			*/
 			static typename Containers::HashMap<std::string, Factory<AbstractProductT>*>::Iterator begin();
-
+			/**
+			* Get an iterator beyond the end of the container of factories.
+			* @return The end iterator of the static hashmap mFactoryTable.
+			*/
 			static typename Containers::HashMap<std::string, Factory<AbstractProductT>*>::Iterator end();
 		protected:
 			/**
-			* Add a concrete factory to the list of factories for this abstract factory.
+			* Add a concrete factory to the list of factories for this abstract factory manager.
 			* @param factoryToAdd A reference to the concrete factory which has to be added.
 			*/
 			static void Add(Factory<AbstractProductT>& factoryToAdd);
 			/**
-			* Remove a concrete factory to the list of factories for this abstract factory.
+			* Remove a concrete factory to the list of factories for this abstract factory manager.
 			* @param factoryToRemove A reference to the concrete factory which has to be removed.
 			*/
 			static void Remove(Factory<AbstractProductT>& factoryToRemove);
 		private:
+			/**
+			* A static hashmap containing pairs of class names (strings) and pointers to their concrete product factories.
+			*/
 			static Containers::HashMap<std::string, Factory<AbstractProductT>*> mFactoryTable;
 		};
 	}
